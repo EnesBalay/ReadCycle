676c443905a37992abe69531f7b69603025244cf
enesbalay-beeNeo <enesbalay@outlook.com>
2024-01-03 10:28:37 +0300

    Login and Register is running

--- Presentation/Controllers/LoginController.cs
+++ Presentation/Controllers/LoginController.cs
@@ -1,6 +1,8 @@
 ﻿using BussinessLayer.Concrete;
+using BussinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
@@ -12,9 +14,21 @@ namespace Turtle.Controllers
     public class LoginController : Controller
     {
         UserManager userManager = new UserManager(new EfUserRepository());
+        [HttpGet]
         [AllowAnonymous]
-        public IActionResult Index()
+        public IActionResult Index(List<ValidationFailure> errors)
         {
+            ViewBag.LoginErrorUser = "";
+            ViewBag.RegisterSuccess = "";
+            ViewBag.RegisterFailed = "";
+            if (errors.Count != 0)
+            {
+                foreach (var item in errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                    System.Console.WriteLine(item.ToString());
+                }
+            }
             var userValues = userManager.GetUserByEmail(User.Identity.Name);
             if (userValues == null)
             {
@@ -36,20 +50,55 @@ namespace Turtle.Controllers
             User datavalue = userManager.GetUserByEmail(p.EmailAddress);
             if (datavalue != null)
             {
-                var claims = new List<Claim>
+                if (datavalue.Password == p.Password)
                 {
-                    new Claim(ClaimTypes.Email, p.EmailAddress)
-                };
-                var useridentity = new ClaimsIdentity(claims, "a");
-                ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
-                await HttpContext.SignInAsync(principal);
-                return RedirectToAction("Index", "Home");
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Email, p.EmailAddress)
+                    };
+                    var useridentity = new ClaimsIdentity(claims, "a");
+                    ClaimsPrincipal principal = new ClaimsPrincipal(useridentity);
+                    await HttpContext.SignInAsync(principal);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.LoginErrorUser = "Kullanıcı adı ya da şifre hatalı!";
+                }
             }
             else
             {
-                ViewBag.LoginErrorUser = "Kullanıcı adı ya da şifre hatalı!";
-                ViewBag.AccountType = "User";
-                ViewBag.EmailAddress = p.EmailAddress;
+                if (p.Name != null || p.Password != null)
+                {
+                    UserValidator uv = new UserValidator();
+                    ValidationResult results = uv.Validate(p);
+                    if (results.IsValid)
+                    {
+                        User user = new User();
+                        user.Name = p.Name;
+                        user.EmailAddress = p.EmailAddress;
+                        user.PhoneNumber = p.PhoneNumber;
+                        user.Password = p.Password;
+                        user.AccountType = "User";
+                        userManager.Add(user);
+                        ViewBag.RegisterSuccess = "Kayıt Başarılı";
+                    }
+                    else
+                    {
+                        foreach (var item in results.Errors)
+                        {
+                            ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                        }
+                        ViewBag.RegisterFailed = "Kayıt Gerçekleşmedi!";
+                    }
+                }
+                else
+                {
+
+                    ViewBag.LoginErrorUser = "Kullanıcı adı ya da şifre hatalı!";
+                    ViewBag.AccountType = "User";
+                    ViewBag.EmailAddress = p.EmailAddress;
+                }
 
             }
             return View();
--- Presentation/Controllers/RegisterController.cs
+++ Presentation/Controllers/RegisterController.cs
@@ -12,48 +12,6 @@ namespace Presentation.Controllers
     {
         UserManager userManager = new UserManager(new EfUserRepository());
         [AllowAnonymous]
-        public IActionResult Index()
-        {
-            var userValues = userManager.GetUserByEmail(User.Identity.Name);
-            if (userValues != null)
-            {
-                return RedirectToAction("Index", "Home");
-            }
-            else
-            {
-                ViewBag.RegisterSuccess = ' ';
-            }
-            return View();
-
-        }
-        [HttpPost]
-        [AllowAnonymous]
-        public IActionResult Index(User newUser)
-        {
-            UserValidator uv = new UserValidator();
-            ValidationResult results = uv.Validate(newUser);
-            if (results.IsValid)
-            {
-                User user = new User();
-                user.Name = newUser.Name;
-                user.EmailAddress = newUser.EmailAddress;
-                user.PhoneNumber = newUser.PhoneNumber;
-                user.Password = newUser.Password;
-                user.AccountType = "User";
-                userManager.Add(user);
-                ViewBag.RegisterSuccess = "Kayıt Başarılı";
-                return RedirectToAction("Index","Login");
-            }
-            else
-            {
-                foreach (var item in results.Errors)
-                {
-                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-                }
-            }
-            return View();
-        }
-        [AllowAnonymous]
         public IActionResult RegisterAjax(User newUser)
         {
             UserValidator uv = new UserValidator();
