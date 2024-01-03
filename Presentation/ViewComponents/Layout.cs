a13fc971b001f2e051d0fd7a37ba4710edbb0d35
Enes Balay <enesbalay@outlook.com>
2024-01-03 23:52:09 +0300

    Layout ViewComponents Added

---
+++ Presentation/ViewComponents/Layout.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Presentation.ViewComponents
+{
+    public class Header:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }    
+    public class Footer:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
